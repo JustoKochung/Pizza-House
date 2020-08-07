dacf644f36dc80b00afb337e93e3c1e3f5cffe2c
JustoKochung <kochungjusto26@gmail.com>
2020-08-07 16:29:58 +0300

    add deliver address function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-var address = prompt("Enter delivery address", "Pizza House")
+var deliveryaddress;
+
+function insert(myForm) {
+  var address = prompt("Enter delivery address", "Enter Delivery Location")
+  document.getElementById('address').value = address;
+  deliveryaddress = address;
+}
